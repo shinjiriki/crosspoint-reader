3062f45ad148af6fd2ac13ae899d8f6956f52df0
Shuba Duck <shinjiriki21@gmail.com>
2026-08-10 17:46:59 +0800

    Update CrossPointSettings.h

--- src/CrossPointSettings.h
+++ src/CrossPointSettings.h
@@ -156,16 +156,7 @@ class CrossPointSettings : public PersistableStore<CrossPointSettings> {
   };
 
   // UI Theme
-  enum UI_THEME {
-    CLASSIC = 0,
-    LYRA = 1,
-    LYRA_3_COVERS = 2,
-    ROUNDEDRAFF = 3,
-    COMPACT = 4,
-    FRAMED = 5,
-    GAME_MENU = 6,
-    NEWSPAPER = 7
-  };
+  enum UI_THEME { CLASSIC = 0, LYRA = 1, LYRA_3_COVERS = 2, ROUNDEDRAFF = 3, GAME_MENU = 4 };
 
   // Image rendering in EPUB reader
   enum IMAGE_RENDERING { IMAGES_DISPLAY = 0, IMAGES_PLACEHOLDER = 1, IMAGES_SUPPRESS = 2, IMAGE_RENDERING_COUNT };
